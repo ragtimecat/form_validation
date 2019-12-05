706b06dd1fe1bef4fc5d3e7db42469b283ef4eba
gleb <gleb573802wow@mail.ru>
2019-12-05 21:07:30 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// Form blur event listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+
+function validateName(e) {
+  const name = e.target;
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
+
+function validateZip(e) {
+  const zip = e.target; //zip code is 5 numbers  with optional dash and 4 numbers
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+
+  if (!re.test(zip.value)) {
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
+  }
+}
+
+function validateEmail(e) {
+  const email = e.target;
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/; // \-\ escaped -
+
+  if (!re.test(email.value)) {
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone(e) {
+  const phone = e.target;
+  const re = /^\+?\d{1}?[-. ]?\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{2}[-. ]?\d{2}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
